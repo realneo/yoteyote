706e8afb942825b942913c054b215cf5371c69de
Neo <Neo@192.168.43.104>
2013-03-12 17:51:59 +0300

    Added the I want / I will forms

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+<html>
+    <head>
+        <title>Yoteyote | Let's work together!</title>
+    </head>
+    <body>
+        <form name='want_post_form' method='post' action='includes/want_post_form_process.php'>
+            I want: <input name='want_input' type='text' value=''/>
+            <button type='submit' name='want_submit_btn'>Post</button>
+        </form>
+        <form name='will_post_form' method='post' action='includes/will_post_form_process.php'>
+            I want: <input name='will_input' type='text' value=''/>
+            <button type='submit' name='will_submit_btn'>Post</button>
+        </form>
+    </body>
+</html>
