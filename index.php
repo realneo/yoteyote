fa369a96d7731b512b537357e491e58bc6da1352
Neo <Neo@192.168.43.104>
2013-03-13 09:12:23 +0300

    Layout Designed

--- index.php
+++ index.php
@@ -1,15 +1,37 @@
 <html>
     <head>
         <title>Yoteyote | Let's work together!</title>
+    	<link href="css/main.css" rel="stylesheet" type="text/css">
     </head>
     <body>
-        <form name='want_post_form' method='post' action='includes/want_post_form_process.php'>
-            I want: <input name='want_input' type='text' value=''/>
-            <button type='submit' name='want_submit_btn'>Post</button>
-        </form>
-        <form name='will_post_form' method='post' action='includes/will_post_form_process.php'>
-            I want: <input name='will_input' type='text' value=''/>
-            <button type='submit' name='will_submit_btn'>Post</button>
-        </form>
+        <div id='left_content'></div><!-- left_content --> 
+        <div id='bottom_logo_strip'></div><!-- bottom_logo_strip -->
+        <div id='top_logo_strip'></div><!-- top_logo_strip -->
+        <div id='logo'><img width='150px' src='images/logo.png' alt='Yoteyote.com' /></div><!-- logo -->
+        <div id='right_content'>
+            <div id='main_content'>
+                <div id='header'>
+                    <a href='#' alt='Home'>Home</a>
+                    <a href='#' alt='How Does it Work?'>How Does it Work?</a>
+                    <a href='#' alt='Help'>Help</a>
+                    <a href='#' alt='Terms'>Terms</a>
+                    <a href='#' alt='About Us'>About Us</a>                   
+                    <a href='#' alt='Any Suggestions?'>Any Suggestions?</a>
+                    <a href='#' alt='Contact Us'>Contact Us</a>
+                </div><!-- header -->
+                <div id='post_forms'>
+                    <table>
+                        <tr>
+                            <td align='right'><span class='want_txt'>I want</span></td>
+                            <td><input type='text' name='want' value='' size='30'/><button name='want_btn' class='want_txt'>create ></button></td>
+                        </tr>
+                        <tr>
+                            <td align='right'><span class='will_txt'>I will</span></td>
+                            <td><input type='text' name='will' value='' size='30'/><button name='want_btn' class='will_txt'>create ></button></td>
+                        </tr>
+                    </table>
+                </div><!-- post_forms -->
+            </div><!-- main_content -->
+        </div><!-- right_content -->
     </body>
 </html>
