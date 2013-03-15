16460d65bf15c3bc8d59339e68c338d733eb2cf2
Neo <Neo@192.168.43.104>
2013-03-15 16:12:46 +0300

    Change of Design

--- index.php
+++ index.php
@@ -1,37 +1,8 @@
 <html>
     <head>
-        <title>Yoteyote | Let's work together!</title>
-    	<link href="css/main.css" rel="stylesheet" type="text/css">
+        <title>Yoteyote | Lets Work Together</title>
     </head>
     <body>
-        <div id='left_content'></div><!-- left_content --> 
-        <div id='bottom_logo_strip'></div><!-- bottom_logo_strip -->
-        <div id='top_logo_strip'></div><!-- top_logo_strip -->
-        <div id='logo'><img width='150px' src='images/logo.png' alt='Yoteyote.com' /></div><!-- logo -->
-        <div id='right_content'>
-            <div id='main_content'>
-                <div id='header'>
-                    <a href='#' alt='Home'>Home</a>
-                    <a href='#' alt='How Does it Work?'>How Does it Work?</a>
-                    <a href='#' alt='Help'>Help</a>
-                    <a href='#' alt='Terms'>Terms</a>
-                    <a href='#' alt='About Us'>About Us</a>                   
-                    <a href='#' alt='Any Suggestions?'>Any Suggestions?</a>
-                    <a href='#' alt='Contact Us'>Contact Us</a>
-                </div><!-- header -->
-                <div id='post_forms'>
-                    <table>
-                        <tr>
-                            <td align='right'><span class='want_txt'>I want</span></td>
-                            <td><input type='text' name='want' value='' size='30'/><button name='want_btn' class='want_txt'>create ></button></td>
-                        </tr>
-                        <tr>
-                            <td align='right'><span class='will_txt'>I will</span></td>
-                            <td><input type='text' name='will' value='' size='30'/><button name='want_btn' class='will_txt'>create ></button></td>
-                        </tr>
-                    </table>
-                </div><!-- post_forms -->
-            </div><!-- main_content -->
-        </div><!-- right_content -->
+        
     </body>
 </html>
