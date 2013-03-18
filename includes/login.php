ab01f6ee504d2164632ae33fe59e0feea8376f1b
Neo <Neo@192.168.43.104>
2013-03-18 23:31:14 +0300

    Login window

---
+++ includes/login.php
@@ -0,0 +1,16 @@
+<form id="login" name="login" method="post" action="includes/login_process.php">
+  <table width="200">
+    <tr>
+        <td align='right'>Username</td>
+        <td><input type='text' name='username' value='' /></td>
+    </tr>
+    <tr>
+        <td align='right'>Password</td>
+        <td><input type='password' name='password' value='' /></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td align="right"><button type='submit' id='login_btn'>Login </button></td>
+    </tr>
+  </table>
+</form>
