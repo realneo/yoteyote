8e0ede9a7f8075fe80fe131c3da2f387bea4a0e0
Neo <Neo@192.168.43.104>
2013-03-23 03:00:03 +0300

    Post process.php

---
+++ includes/post_process.php
@@ -0,0 +1,24 @@
+<?php
+    session_start();
+    include('db_conn.php');
+    
+    // Getting values from the Form
+    $post = mysql_escape_string($_POST['post']);
+    $description = mysql_escape_string($_POST['description']);
+    $type = mysql_escape_string($_POST['type']);
+    $currency = mysql_escape_string($_POST['currency']);
+    $amount = mysql_escape_string($_POST['amount']);
+
+    $date_time = date('Y-m-d H:i:s');
+    
+    $user_id = $_SESSION['user_id'];
+    
+    $q = mysql_query("INSERT INTO `posts` (`id`, `post`, `description`, `type`, `date`, `amount`, `currency`, `user_id`) 
+        VALUES ('NULL', '$post', '$description', '$type', '$date_time', '$amount', '$currency', '$user_id')");
+    if($q){
+        echo'success';
+    }else{
+        echo'failed';
+    }
+            
+?>
