5eb2a8883831c8000fcc06c38ee495a6187ed9b7
Neo <Neo@192.168.43.104>
2013-04-07 05:09:32 +0300

    Fixed the comma problem when posting

--- includes/post_process.php
+++ includes/post_process.php
@@ -8,13 +8,14 @@
     $type = mysql_escape_string($_POST['type']);
     $currency = mysql_escape_string($_POST['currency']);
     $amount = mysql_escape_string($_POST['amount']);
+    $f_amount = str_replace(",", "", $amount); 
 
     $date_time = date('Y-m-d H:i:s');
     
     $user_id = $_SESSION['user_id'];
     
     $q = mysql_query("INSERT INTO `posts` (`id`, `post`, `description`, `type`, `date`, `amount`, `currency`, `user_id`) 
-        VALUES ('NULL', '$post', '$description', '$type', '$date_time', '$amount', '$currency', '$user_id')");
+        VALUES ('NULL', '$post', '$description', '$type', '$date_time', '$f_amount', '$currency', '$user_id')");
     if($q){
         echo'success';
     }else{
