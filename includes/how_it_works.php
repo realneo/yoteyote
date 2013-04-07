9a792fed77819eaeaaf2b92a212404a39f9804ec
Nadhir <Administrator@Neos-MacBook-Pro.local>
2013-04-07 15:39:49 +0300

    Popup window Artwork

---
+++ includes/how_it_works.php
@@ -0,0 +1,138 @@
+<style type="text/css">
+#popup_window_hiw{
+	position:absolute;
+	width:817px;
+	overflow:hidden;
+	height:522px;
+	background-color:#fff;
+	border:1px solid #ccc;
+	border-radius:5px;
+}
+#title_hiw{
+	font-weight:bold;
+	font-family:couture;
+	font-size:22px;
+	padding-top:22px;
+	padding-left:45px;
+}
+#image_footer_hiw{
+	position:absolute;
+	bottom:0px;
+}
+#close_hiw{
+	position:absolute;
+	right:40px;
+	top:0px;
+	background-color:#990000;
+	padding:3px 15px;
+	font-family:arial;
+	font-weight:bold;
+	color:#fff;
+	cursor:pointer;
+}
+#gotit_btn{
+	background-color:#9D4848;
+	position:absolute;
+	font-size:18px;
+	font-weight:bold;
+	color:#fff;
+	padding:11px;
+	font-family:couture;
+	border-bottom:4px solid #990000;
+	position:absolute;
+	right:40px;
+	bottom:60px;
+	cursor:pointer;
+}
+.circle_number{
+	background-color:#000;
+	color:#fff;
+	padding:5px 20px;
+	border-radius:60px;
+	font-family:couture;
+	font-size:43;
+	font-weight:bold;
+}
+#info_2 .circle_number{
+	padding:7px 18px;	
+}
+#info_2{
+	padding-top:70px;
+}
+#info_3{
+	padding-top:70px;	
+}
+#info_3 .circle_number{
+	padding:7px 17px;
+}
+#yy{
+	postion:absolute;
+	padding-top:22px;
+	padding-left:50px;
+}
+.header{
+	font-family:calibri;
+	font-size:20px;
+	color:#990000;
+	font-weight:bold;
+	position:absolute;	
+}
+#description_1{
+	font-family:calibri;
+	font-size:13px;
+	color:#000;
+	font-weight:light;
+	font-style:italic;
+	position:absolute;
+	padding-top:5px;
+}
+#description_header_1{
+	padding-left:70px;
+}
+#description_header_2{
+	padding-left:70px;
+}
+#description_header_3{
+	padding-left:70px;
+}
+
+#code{
+	font-family:hand of sean;
+	text-shadow: 1px 2px 2px #000;
+	color:#ccc;
+	font-size:30px;
+	padding-top:540px;
+	padding-left:640px;
+	cursor:pointer;
+	
+}
+
+#right_content{
+	position:absolute;
+	right:25px;
+	width:400px;
+	height:400px;
+}
+
+</style>
+    <div id='popup_window_hiw'>
+        <div id='title_hiw'>HOW IT WORKS</div>
+        <div id='close_hiw'><a class="link1" >X</a></div>
+        <div id='image_footer_hiw' ><img src="../images/dar_city.png" alt="*"> </div>
+        <div id='gotit_btn' style='cursor:pointer;'><a class='link2'>OK .. GOT IT<a></div> 
+        <div id='right_content'>
+            <div id='info_1'><span class='circle_number'>1</span></div>
+                <div id='description_header_1'> <span class='header'>Drop your wants/ Wills</span> 
+                    <br><div id='description_1'>example " i will like your facebook page and follow your twitter account "</div> 	
+                </div>
+                <div id='info_2'><span class='circle_number'>2</span></div>
+                <div id='description_header_2'> <span class='header'>Put a price and your contact details</span> 
+                    <br><div id='description_1'>Set a price , or upload a picture - coming soon</div> 	
+                </div>
+                <div id='info_3'><span class='circle_number'>3</span></div>
+                <div id='description_header_3'> <span class='header'>Then Post... Thats it! </span></div>						
+        </div><!-- right content-->
+        <div id="left_content" > 
+                <div id='yy'><img src="images/yyartwork.png" alt="*"></div>
+        </div> <!--left content-->
+    </div> <!-- popup_window -->
