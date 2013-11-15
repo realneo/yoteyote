4326296e844a0af22d9c40c6a09682345da767ca
Nadhir Bahayan <nbahayan@gmail.com>
2013-11-15 21:00:07 +0300

    Erasing all the Files

--- includes/logout_process.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION['user_id']);
-    unset($_SESSION['first_name']);
-    unset($_SESSION['last_name']);
-    session_destroy();
-    header('Location: ../index.php');
-?>
