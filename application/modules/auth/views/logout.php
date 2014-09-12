20a7a9c467e27e6b77c64b50ce2a86b5526115cf
realneo <neo@yoteyote.com>
2014-09-12 17:51:27 +0300

    Deleted all From Previous Branch

--- application/modules/auth/views/logout.php
+++
@@ -1,8 +0,0 @@
-<div id="login">
-
-	<div class="alert alert-success alert-dismissable">
-		<h2>Logout</h2>
-		<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-		<strong>You have been successfully logged out!</strong>
-	</div
-</div>
