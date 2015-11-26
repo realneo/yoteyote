cc8154cb34bdad12ec4ba379aa978e3036be0cb8
Nadhir Mubarak Bahayan <neo@yoteyote.com>
2015-11-26 23:10:24 +0300

    Removed the Categories Selection

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 
 		<div class='row'>
 			<div class='col-md-12'>
+					<!--
 					<div class="field_select">
 						<select class="select_styled select_styled_red">
 							<option value="0">Select Category</option>
@@ -19,7 +20,7 @@
 							<option value="<?php echo 'http://localhost:8888/yoteyote_new/index.php?category_id='.$category['category_id']; ?>"><?php echo $category['category_name']; ?></option>
 							<?php } ?>
 						</select>
-					</div>
+					</div>-->
                    Sort
                     <a href="?post_type=wills" class="btn <?php if($_GET['post_type'] == 'wills'){ echo 'btn-red'; }else{ echo '';} ?> btn-left btn-acute"><span>Wills</span></a>
 					<a href="?post_type=all" class="btn <?php if(!isset($_GET['post_type']) OR $_GET['post_type'] == 'all'){ echo 'btn-red'; }else{echo '';}?> btn-acute"><span>All</span></a>
